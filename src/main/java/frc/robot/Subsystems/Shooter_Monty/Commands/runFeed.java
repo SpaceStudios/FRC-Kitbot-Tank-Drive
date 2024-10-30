52a7e39082d35afd2aea1ddbfbe35e04310e1e16
SpaceStudios <109873883+SpaceStudios@users.noreply.github.com>
2024-10-30 15:48:35 +0000

    Added Feed Command

--- src/main/java/frc/robot/Subsystems/Shooter_Monty/Commands/runFeed.java
+++ src/main/java/frc/robot/Subsystems/Shooter_Monty/Commands/runFeed.java
@@ -5,18 +5,27 @@
 package frc.robot.Subsystems.Shooter_Monty.Commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.Shooter_Monty.MontySubsystem;
 
 public class runFeed extends Command {
   /** Creates a new runFeed. */
-  public runFeed() {
+  public final MontySubsystem mainSubsystem;
+  public final double speed;
+  public runFeed(MontySubsystem mainMontySubsystem, double newSpeed) {
     // Use addRequirements() here to declare subsystem dependencies.
+    mainSubsystem = mainMontySubsystem;
+    speed = newSpeed;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    mainSubsystem.setFeederVolts(speed);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    mainSubsystem.setFeederVolts(0);
+  }
 }
