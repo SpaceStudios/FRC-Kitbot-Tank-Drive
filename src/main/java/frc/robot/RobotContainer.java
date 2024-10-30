3602f87cbba5f29b68150a95ab62f713640fea23
SpaceStudios <109873883+SpaceStudios@users.noreply.github.com>
2024-10-30 16:25:27 +0000

    Added Controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -11,6 +12,12 @@ import frc.robot.Subsystems.Drivetrain.DrivetrainSubsystem;
 import frc.robot.Subsystems.Shooter.ShooterSubsystem;
 import frc.robot.Subsystems.Shooter.Commands.FeederExecute;
 import frc.robot.Subsystems.Shooter.Commands.ShooterExecute;
+import frc.robot.Subsystems.Shooter_Monty.MontySubsystem;
+import frc.robot.Subsystems.Shooter_Monty.Commands.ToggleHood;
+import frc.robot.Subsystems.Shooter_Monty.Commands.ToggleIntake;
+import frc.robot.Subsystems.Shooter_Monty.Commands.runFeed;
+import frc.robot.Subsystems.Shooter_Monty.Commands.runIntake;
+import frc.robot.Subsystems.Shooter_Monty.Commands.runLauncher;
 
 public class RobotContainer {
   public RobotContainer() {
@@ -30,9 +37,20 @@ public class RobotContainer {
           drivetrainSubsystem.setVoltagesArcadeCommand(
             () -> modifyJoystick(-controller.getLeftY()),
             () -> modifyJoystick(controller.getRightX())));
-        controller.rightTrigger().whileTrue(new ShooterExecute(-1*Constants.ShooterStrength, Shooter)).whileTrue(new FeederExecute(-1*Constants.ShooterStrength, Shooter));
-        controller.leftTrigger().whileTrue(new FeederExecute(1, Shooter));
-        controller.b().whileTrue(new ShooterExecute(-1*Constants.ShooterStrength, Shooter));
+        switch (Constants.CurrentType) {
+          case Kitbot:
+            controller.rightTrigger().whileTrue(new ShooterExecute(-1*Constants.ShooterStrength, Shooter)).whileTrue(new FeederExecute(-1*Constants.ShooterStrength, Shooter));
+            controller.leftTrigger().whileTrue(new FeederExecute(1, Shooter));
+            controller.b().whileTrue(new ShooterExecute(-1*Constants.ShooterStrength, Shooter));
+            break;
+          case Monty:
+            controller.rightTrigger().whileTrue(new runFeed(Monty, -1));
+            controller.leftBumper().onTrue(new ToggleIntake(Monty));
+            controller.rightBumper().onTrue(new ToggleHood(Monty));
+            controller.leftTrigger().whileTrue(new runIntake(Monty, 1));
+            controller.y().whileTrue(new runLauncher(Monty, -1));
+            break;
+        }
         break;
       case cursedController:
         drivetrainSubsystem.setDefaultCommand(
@@ -55,6 +73,7 @@ public class RobotContainer {
 
   DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem();
   ShooterSubsystem Shooter = new ShooterSubsystem();
+  MontySubsystem Monty = new MontySubsystem(new PneumaticHub(31));
 
   private double modifyJoystick(double in) {
     if (Math.abs(in) < 0.05) {
